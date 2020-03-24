a061e73bebf493f7737a2d6e8dcf4538af068aef
saurabh-619 <saurabhbomble107@gmail.com>
2020-03-24 13:05:44 +0530

    added notes.js and some functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import Header from './Header';
 import Note from './Note'
 import { Footer } from './Footer';
-
+import  notes from '../notes';
+ 
 const App = () => {
     return(
     <div>
         <Header/>
-        <Note/>
+        {notes.map( note => {
+            return(
+                <Note 
+                    key={note.key}
+                    notes={note.title} 
+                    content={note.content}
+                />
+            )
+        })}
         <Footer/>
     </div>);
 }
